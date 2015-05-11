5d7ede05f425524b0c57eca3206ba01460aed4a4
Lch315 <156894161@qq.com>
2015-05-12 00:16:36 +0800

    .

--- js/fade.js
+++ js/fade.js
@@ -1,10 +1,10 @@
 (function($) {
-	var fade = function(element,option) {
-		this.option = $.extend({}, this.defOptions, option);
-		this.init(element, this.option);
+	var Fade = function(element, options) {
+		this.defOptions = $.extend({}, this.defOptions, options);
+		this.init(element);
 	};
 
-	fade.prototype = {
+	Fade.prototype = {
 
 		defOptions: {
 			speed : 500,
@@ -15,18 +15,54 @@
 			item : ">li"
 		},
 
-		init: function(element, option) {
+		defStatus: {
+			index: 0,
+			lastIndex: 0,
+			len: 0,
+			timer: null
+		},
+
+		init: function(element) {
 			var that = this,
-				ul = element.find(option.items),
-				li = ul.find(option.item),
-				len = li.length,
+				ul = element.find(this.defOptions.items),
+				li = ul.find(this.defOptions.item),
 				elWidth = element.width(),
 				elHeight = element.height();
 
-			element.css({"position":"relative", "overflow":"hidden"});
-			li.css("position","absolute");
+			this.defStatus.len = li.length,
+
+			element.css("position", "relative");
+			li.css({"position":"absolute", "display":"none"});
+			li.first().css("display", "block");
+			li.width(elWidth);
+			li.height(elHeight);
+			//debugger
+			// dots && createDots();
+			this.defOptions.autoplay && setTimeout(this.goFade(that, li),this.defOptions.delay);
+		},
+
+		goFade: function(that, item) {
+			var index = that.defStatus.index,
+				len = that.defStatus.len,
+				lastIndex = that.defStatus.lastIndex,
+				speed = that.defOptions.speed;
+			if (index < len) {
+				index ++;
+			} else {
+				index = 0;
+			}
+
+			item.eq(index).css("z-index", 2);
+			item.eq(lastIndex).css("z-index", 1).fadeOut(speed, function() {
+				item.eq(index).fadeIn(speed);
+			});
+
+			that.defStatus.lastIndex = index;
+
+			setTimeout(arguments.callee(that, item), that.defOptions.delay);	
 		}
 	};
 
-})(jQuery)
+	window.Fade = Fade || {};
 
+})(jQuery)
